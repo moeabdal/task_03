578436431e6423416ed2a5413237815e542199ba
moeabdal <mohammed.h.abdal@gmail.com>
2019-07-09 21:06:21 +0300

    First commit

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,20 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+    	"my_list" : [
+    		{
+    			"restaurant_name" : "Sakura",
+    			"food_type" : "Sushi"
+    		}, 
+    		{
+    			"restaurant_name" : "Burger Boutique",
+    			"food_type" : "Burgers"
+    		}, 
+    		{
+    			"restaurant_name" : "Terrace Grill",
+    			"food_type" : "Steaks"
+    		}
+    	]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +27,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    	"my_object" : {
+    		"restaurant_name" : "Sakura",
+    		"food_type" : "Sushi"
+    	}
 
     }
     return render(request, 'detail.html', context)
